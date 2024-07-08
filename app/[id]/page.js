940ac67bbedec210c158bf5a8e5b6254e4b5ea0a
dp <codewithdp2073@gmail.com>
2024-07-08 17:39:16 +0530

    API Calling & App Routing

---
+++ app/[id]/page.js
@@ -0,0 +1,22 @@
+"use client"
+import axios from 'axios'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+
+const page = ({params}) => {
+   const {id}=params
+   const [users, setusers] = useState([])
+   const getUsers = async () => {
+     const {data}  = await axios.get("https://jsonplaceholder.typicode.com/users/" + id)
+     setusers(data)
+     console.log(data)
+   }
+ useEffect(() => {
+   getUsers()
+ }, [])
+  return (
+    <div>{JSON.stringify(users)}</div>
+  )
+}
+
+export default page
--- app/page.js
+++ app/page.js
@@ -1,10 +1,27 @@
-import React from 'react'
+"use client"
+import axios from 'axios'
 import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+
 const page = () => {
+  const [users, setusers] = useState([])
+  const getUsers = async () => {
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+    setusers(data)
+    console.log(data)
+  }
+useEffect(() => {
+  getUsers()
+}, [])
+
   return (
     <>
-    <h1 className='text-2xl font-bold '>This is a Home Page.</h1>
-    <Link href='/Contact'>Contact</Link>
+      <button onClick={getUsers} className='text-white font-bold px-4 py-2 rounded bg-green-600'>Get Data</button>
+      <div className='bg-slate-100 p-8 mt-5'>
+        <ul>{users.map((e)=>{
+          return <li>{e.username} ---<Link href={`${e.id}`}>Explore</Link></li>
+        })}</ul>
+      </div>
     </>
   )
 }
